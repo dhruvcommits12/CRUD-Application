4c14b9eb482794124fc131e1a52047b1e41a9aa1
Dhruv Gupta <dhruvstayz12@gmail.com>
2026-07-02 18:46:35 +0530

    database schema

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express")
+
+const users=[]
+
+const organizations = []
+
+const boards = []
+
+const issues = []
+
+//It's a good idea to have id's as the uniqueness constraint 
+
+const app = express()
+
+app.listen(3000)
