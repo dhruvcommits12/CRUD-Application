804a5cf654911623663f1bed9cc19549a37279a2
Dhruv Gupta <dhruvstayz12@gmail.com>
2026-07-09 13:07:17 +0530

    designed the routes

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 const express=require("express")
 
-const users=[]
+const USERS=[]
 
-const organizations = []
+const ORGANIZATIONS = []
 
-const boards = []
+const BOARDS = []
 
-const issues = []
+const ISSUES = []
 
 //It's a good idea to have id's as the uniqueness constraint 
 
 const app = express()
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+    
+})
+
+app.post("/org",(req,res)=>{
+    
+})
+
+app.post("/signup",(req,res)=>{
+    
+})
+
+app.post("/add-member-to-org",(req,res)=>{
+
+})
+
+app.post("/board",(req,res)=>{
+    
+})
+
+app.post("/issue",(req,res)=>{
+    
+})
+
+app.get("/boards",(req,res)=>{
+
+})
+
+app.get("/issues",(req,res)=>{
+
+})
+
+app.get("/members",(req,res)=>{
+
+})
+
+app.put("/issues",(req,res)=>{
+
+})
+
+app.delete("/members",(req,res)=>{
+    
+})
 
 app.listen(3000)
